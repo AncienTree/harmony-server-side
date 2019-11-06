87544dafdf60da137cf1c777439583bc21eab418
Mateusz Dąbek <m.dabek@entpoint.pl>
2019-11-06 20:38:51 +0100

    Init commit

---
+++ harmony-schedule/src/main/java/pl/entpoint/harmony/HarmonyScheduleApplication.java
@@ -0,0 +1,13 @@
+package pl.entpoint.harmony;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HarmonyScheduleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HarmonyScheduleApplication.class, args);
+	}
+
+}
