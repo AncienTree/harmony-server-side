d184a5f08637361f399b1d4629aee17e723ba3bb
Mateusz Dąbek <m.dabek@entpoint.pl>
2019-11-08 15:50:45 +0100

    Zastąpienie DAO przez JpaRepository.

--- harmony-schedule/src/main/java/pl/entpoint/harmony/service/impl/TestUserServiceImpl.java
+++ harmony-schedule/src/main/java/pl/entpoint/harmony/service/impl/TestUserServiceImpl.java
@@ -1,39 +1,48 @@
 package pl.entpoint.harmony.service.impl;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import pl.entpoint.harmony.dao.TestUserDao;
 import pl.entpoint.harmony.entity.User;
+import pl.entpoint.harmony.repository.TestUserRepository;
 import pl.entpoint.harmony.service.TestUserService;
 
 @Service
 public class TestUserServiceImpl implements TestUserService{
 
 	@Autowired
-	TestUserDao testUserDao;
+	TestUserRepository testUserRepository;
 	
 	@Override
 	public List<User> getCustomers() {
 		
-		return testUserDao.getCustomers();
+		return testUserRepository.findAll();
 	}
 
 	@Override
 	public void saveCustomer(User theCustomer) {
-		testUserDao.saveCustomer(theCustomer);		
+		testUserRepository.save(theCustomer);		
 	}
 
 	@Override
 	public User getCustomer(int theId) {
-		return testUserDao.getCustomer(theId);
+		Optional<User> result = testUserRepository.findById(theId);
+		
+		User theUser = null;
+		if(result.isPresent()) {
+			theUser = result.get();
+		} else {
+			throw new RuntimeException("Nie znaleziono użytkownika pod ID - " + theId);
+		}
+		return theUser;
 	}
 
 	@Override
 	public void deleteCustomer(int theId) {
-		testUserDao.deleteCustomer(theId);
+		testUserRepository.deleteById(theId);
 		
 	}
 	
