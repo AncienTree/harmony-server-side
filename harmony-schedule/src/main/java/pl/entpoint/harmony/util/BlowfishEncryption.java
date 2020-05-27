e596b34935d2289a44c280c9a2905610027824f4
Mateusz Dąbek <mate.dab@gmail.com>
2020-05-27 10:54:23 +0200

    Zmiana w metodach szyforwania Blowfish

--- harmony-schedule/src/main/java/pl/entpoint/harmony/util/BlowfishEncryption.java
+++ harmony-schedule/src/main/java/pl/entpoint/harmony/util/BlowfishEncryption.java
@@ -1,9 +1,12 @@
 package pl.entpoint.harmony.util;
 
 import javax.crypto.spec.SecretKeySpec;
+
+import java.util.Base64;
+
 import javax.crypto.Cipher;
 
-import org.apache.tomcat.util.codec.binary.Base64;
+import lombok.extern.slf4j.Slf4j;
 
 /**
  * @author Mateusz Dąbek
@@ -11,27 +14,45 @@ import org.apache.tomcat.util.codec.binary.Base64;
  * 
  */
 
+@Slf4j
 public class BlowfishEncryption {
-	
-	//TODO Zmienić i ukryć w finalnej wersji
-	private static final String KEY = "Test";
-    private static Base64 base64 = new Base64(true);
-    
-    public static String encrypt(String data) throws Exception {
-    	SecretKeySpec key = new SecretKeySpec(KEY.getBytes("UTF8"), "Blowfish");
-    	Cipher cipher = Cipher.getInstance("Blowfish");
-    	cipher.init(Cipher.ENCRYPT_MODE, key);
-    	    	
-    	return base64.encodeToString(cipher.doFinal(data.getBytes("UTF8")));
-    }
-    
-    public static String decrypt(String encryptedData)  throws Exception {
-    	byte[] encrypted = base64.decodeBase64(encryptedData);
-        SecretKeySpec key = new SecretKeySpec(KEY.getBytes("UTF8"), "Blowfish");
-        Cipher cipher = Cipher.getInstance("Blowfish");
-        cipher.init(Cipher.DECRYPT_MODE, key);
-        byte[] decrypted = cipher.doFinal(encrypted);
-        
-    	return new String(decrypted);
-    }
+	// TODO Zmienić i ukryć w finalnej wersji
+	private static final String KEY = "Test";;
+
+	public static String encrypt(String stringToEncrypt) {
+		log.info("Rozpoczęcie szyfrowania.");
+		try {
+			byte[] keyData = (KEY).getBytes();
+			SecretKeySpec keySpec = new SecretKeySpec(keyData, "Blowfish");
+			Cipher cipher = Cipher.getInstance("Blowfish");
+			cipher.init(Cipher.ENCRYPT_MODE, keySpec);
+			byte[] hasil = cipher.doFinal(stringToEncrypt.getBytes());
+
+			return new String(Base64.getEncoder().encode(hasil));
+		} catch (Exception e) {
+			log.error("Błąd w trakcie szyfrowania. " + e.getMessage());
+			e.printStackTrace();
+
+			return null;
+		}
+	}
+
+	public static String decrypt(String encryptedString) {
+		log.info("Rozpoczęcie deszyforwania.");
+		try {
+			byte[] keyData = (KEY).getBytes();
+			SecretKeySpec keySpec = new SecretKeySpec(keyData, "Blowfish");
+			Cipher cipher = Cipher.getInstance("Blowfish");
+			cipher.init(Cipher.DECRYPT_MODE, keySpec);
+			byte[] hasil = cipher.doFinal(Base64.getDecoder().decode(encryptedString));
+
+			return new String(hasil);
+
+		} catch (Exception e) {
+			log.error("Błąd w trakcie deszyforwania. " + e.getMessage());
+			e.printStackTrace();
+
+			return null;
+		}
+	}
 }
