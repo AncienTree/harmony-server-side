13426a1c59cab7fcddc75cfa3c0cc8e8eaed2d14
Mateusz Dąbek <mate.dab@gmail.com>
2020-07-16 11:59:20 +0200

    Fix walidacja w encji MonthHours

--- src/main/java/pl/entpoint/harmony/entity/settings/MonthHours.java
+++ src/main/java/pl/entpoint/harmony/entity/settings/MonthHours.java
@@ -31,50 +31,38 @@ public class MonthHours extends AuditEntity implements Serializable {
     private String year;
 
     @NotNull
-    @Size(max = 4)
     private int january;
     
     @NotNull
-    @Size(max = 4)
     private int february;
     
     @NotNull
-    @Size(max = 4)
     private int march;
     
     @NotNull
-    @Size(max = 4)
     private int april;
     
     @NotNull
-    @Size(max = 4)
     private int may;
     
     @NotNull
-    @Size(max = 4)
     private int june;
     
     @NotNull
-    @Size(max = 4)
     private int july;
     
     @NotNull
-    @Size(max = 4)
     private int august;
     
     @NotNull
-    @Size(max = 4)
     private int september;
     
     @NotNull
-    @Size(max = 4)
     private int october;
     
     @NotNull
-    @Size(max = 4)
     private int november;
     
     @NotNull
-    @Size(max = 4)
     private int december;
 }
