82df87e358433cad96846cbee60facf7b1163e0b
Mateusz Dąbek <mate.dab@gmail.com>
2020-07-17 12:48:43 +0200

    Nowe pola w SimpleEmployee

--- src/main/java/pl/entpoint/harmony/entity/dto/SimpleEmployee.java
+++ src/main/java/pl/entpoint/harmony/entity/dto/SimpleEmployee.java
@@ -6,6 +6,7 @@ import pl.entpoint.harmony.entity.employee.Employee;
 import pl.entpoint.harmony.entity.employee.enums.WorkStatus;
 
 import java.io.Serializable;
+import java.time.LocalDate;
 
 @Getter @Setter
 public class SimpleEmployee implements Serializable {
@@ -14,11 +15,32 @@ public class SimpleEmployee implements Serializable {
     private String fullName;
     private String position;
     private WorkStatus workStatus;
+    private float etat;
+    private LocalDate startWorkDate;
+    private LocalDate endWorkDate;
+    private String userSection;
+    private String userLine;
+    private String fte;
+    private String fteStart;
 
     public SimpleEmployee(Employee employee) {
         id = employee.getId();
         fullName = employee.getLastName() + " " + employee.getFirstName();
         position = employee.getPosition();
         workStatus = employee.getWorkStatus();
+
+        startWorkDate = employee.getStartWorkDate();
+        endWorkDate = employee.getEndWorkDate();
+        userSection = employee.getEmployeeDetails().getUserSection();
+        userLine = employee.getEmployeeDetails().getUserLine();
+        fte = employee.getEmployeeDetails().getFte();
+        fteStart = employee.getEmployeeDetails().getFteStart();
+        
+        if (employee.getEtat() == null) {
+        	etat = -1;
+        } else {
+        	etat = employee.getEtat();
+        }
+        
     }
 }
