2b3173e22e5ce5874f3726f662dda60764230b18
Mateusz Dąbek <mate.dab@gmail.com>
2020-09-08 13:52:55 +0200

    Stats

--- src/main/java/pl/entpoint/harmony/service/statistics/StatsServiceImpl.java
+++ src/main/java/pl/entpoint/harmony/service/statistics/StatsServiceImpl.java
@@ -1,9 +1,16 @@
 package pl.entpoint.harmony.service.statistics;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import pl.entpoint.harmony.entity.dto.Stats;
+import pl.entpoint.harmony.entity.user.User;
+import pl.entpoint.harmony.service.schedule.record.ScheduleRecordService;
+import pl.entpoint.harmony.service.settings.monthHours.MonthHoursService;
+import pl.entpoint.harmony.service.user.UserService;
 
 import java.security.Principal;
+import java.time.LocalDate;
+import java.util.Optional;
 
 /**
  * @author Mateusz Dąbek
@@ -13,21 +20,50 @@ import java.security.Principal;
 @Service
 public class StatsServiceImpl implements StatsService {
 
-    @Override
-    public Stats getMyStats(Principal principal) {
+	private static LocalDate date = LocalDate.now();
+	
+	private final UserService userService;
+	private final ScheduleRecordService recordService;
+	private final MonthHoursService mHoursService;
+	
+	
+	@Autowired
+	public StatsServiceImpl(UserService userService, ScheduleRecordService recordService,
+			MonthHoursService mHoursService) {
+		this.userService = userService;
+		this.recordService = recordService;
+		this.mHoursService = mHoursService;
+	}
 
-        // Hardcoded test
-        Stats stats = new Stats();
-        stats.setAbsences(1);
-        stats.setCurrentMonthAbsences(2);
-        stats.setCurrentMonthLeaves(12);
-        stats.setCurrentMonthWorkedHours(140);
+	@Override
+	public Stats getMyStats(Principal principal) {
+		User user;
+		Stats stats = new Stats();
+		
+		Optional<User> optUser = Optional.ofNullable(userService.getUserByLogin(principal.getName()));
+		if(optUser.isPresent()) {
+			user = optUser.get();
+		} else {
+			throw new IllegalArgumentException("Nie znaleziono użytkownika pod takim loginem: " + principal.getName());
+		}		
+		
+		stats.setLeaves(user.getEmployee().getEmployeeLeave().getTotal());
+		stats.setCurrentMonthHours(mHoursService.checkMonthHours(date));
+		
 
-        return stats;
-    }
+//		private int currentMonthWorkedHours;
+//	    private int currentMonthHours;
+//		private int currentMonthLeaves;
+//		private int absences;
+//		private int currentMonthAbsences
 
-    @Override
-    public Stats getStats(Long id) {
-        return null;
-    }
+		return stats;
+	}
+
+
+
+	@Override
+	public Stats getStats(Long id) {
+		return null;
+	}
 }
